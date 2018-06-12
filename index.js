a11a9a2fc10cf516ad7a8d847f9c9546de711ce3
Rachel Hicken <rachkh22@gmail.com>
2018-06-12 12:05:24 -0600

    Following steps, adding front end

--- index.js
+++ index.js
@@ -3,21 +3,72 @@
 
 const { GraphQLServer } = require('graphql-yoga')
 
+//Types are what makes graphql stand out over REST.  
+//We need to define every piece of data we're going to use
+//The structure of a Type :
+/*
+    type NAME - This declares a type and names it, just like a class
+        Then inside the type we declare our properties
+    ! - Required
+    [] = Array
+*/
 
-const typeDefs = 
-`type Query {
+
+const typeDefs =
+    `type Query {
     welcome: String!
-}`
+    links: [Link!]!
+}
 
+type Mutation {
+    addLink(url: String!, description: String!): Link!
+}
 
-//This object needs to match the structure of our typeDefinition
-//All values should be functions and what they return is like doing a res.send
+type Link {
+    id: ID!
+    description: String!
+    url: String!
+}
+`
+//GraphQL allows parameters to be added to a key
+//This is just some dummy data.  In a real app we'd use a database instead
+let articleLinks = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'A resources to learn graphql. Check out the advanced sections for some more in-depth tutorials.'
+}, {
+    id: 'link-1',
+    url: 'news.ycombinator.com',
+    description: 'Hacker news is like reddit that doesn\'t suck.  Focused on tech.  Great place to improvey our chameleon skills.'
+}, {
+    id: 'link-2',
+    url: 'https://www.graphqlhub.com/',
+    description: 'Some practice APIs to play around with queries'
+}]
+
+let idCount = articleLinks.length
+//This object needs to match the structure of our typeDefinition Queries and Mutations
+//All values should be functions and what they return is like doing a res.send...almost
+
 const resolvers = {
     Query: {
-        welcome: ()=>`Hacker News clone begins.`
+        welcome: () => `Hacker News clone begins.`,
         //could still call function from other files, etc.
-    }
+        links: () => articleLinks,
+    },
+    Mutation: {
+        addLink: (root, args) => { //root is for context, args is for params coming in
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            articleLinks.push(link)
+            return link; //like res.send
+        }
+    },
 }
+//res.send happens behind the scenes automatically
 
 //Our server is looking for our typeDefs and our Resolvers
 const server = new GraphQLServer({
@@ -26,3 +77,5 @@ const server = new GraphQLServer({
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
+
+//node index.js to run the sandbox
